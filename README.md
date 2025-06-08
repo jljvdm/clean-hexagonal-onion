7dbb3138ba3724dbbb7fbba20918196d457f9850
Jeroen van der Meer sr <jeroen@quovis.nl>
2025-06-08 17:31:55 +0200

    Einde hoofdstuk 2

---
+++ README.md
@@ -0,0 +1,9 @@
+# J-Spring 2025 Workshop
+
+## Informatie
+
+[Informatie Workshop Mike Kingma](https://maikkingma.github.io/clean-hexagonal-onion-docs/docs/intro)
+
+## Veranderingen
+
+- Java 24
---
+++ src/main/java/eu/javaland/clean_hexagonal_onion/CleanHexagonalOnionApplication.java
@@ -0,0 +1,11 @@
+package eu.javaland.clean_hexagonal_onion;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanHexagonalOnionApplication {
+    
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(CleanHexagonalOnionApplication.class, args);
+    }
+}
